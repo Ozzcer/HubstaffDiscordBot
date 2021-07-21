4e53e311cd098752acb077459f254b5fa1eafb04
Oz <oharrison1998@outlook.com>
2021-07-21 16:14:00 +0100

    Command structure defined

--- bot.py
+++ bot.py
@@ -1,7 +1,32 @@
+from asyncio.windows_events import NULL
 from dependencies.dotenv.main import dotenv_values
 from dependencies import discord
 
 config = dotenv_values(".env")
+
+try:
+    BOT_TOKEN = config['BOT_TOKEN']
+except:
+    raise KeyError("BOT_TOKEN not set in environment variables")
+
+
+def report(command):
+    try:
+        # TODO implement report command
+        return 'Report Command Successfully Called'
+    except:
+        return False
+
+
+def commandNotRecognised(command):
+  return 'Command Not Recognised'
+
+
+def commandSwitch(command):
+    return {
+        'report': report,
+    }.get(command[1], commandNotRecognised)    # Command Not Recognised is default
+
 
 client = discord.Client()
 
@@ -17,8 +42,14 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hubstaff'):
-      command = message.content
-      command.split()
-      #TODO switch over commands and carry them out
+        message_text = message.content
+        command = message_text.split()
+        print(command)
+        command_function = commandSwitch(command)
+        try:
+          await message.channel.send(command_function(command))
+        except:
+          await message.channel.send('Unknown error attempting to run command function')
+          raise Exception('Unknown error attempting to run command function')
 
 client.run(config['BOT_TOKEN'])
